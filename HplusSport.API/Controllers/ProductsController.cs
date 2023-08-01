418e1411e2f1f8c18945b9ac332f1d496b4abe98
adamjmcwilliams <adamjmcwilliams@gmail.com>
2023-08-01 14:43:08 -0700

    Add project files.

---
+++ HplusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using HplusSport.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HplusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllProducts()
+        {
+            return Ok(_context.Products.ToArray());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ HplusSport.API/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using HPlusSport.API.Models;
+using System.Text.Json.Serialization;
+
+public class Product
+{
+	public int Id { get; set; }
+	public string Sku { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
+	public decimal Price { get; set; }		
+	public bool IsAvailable { get; set; }
+
+    public int CategoryId { get; set; }
+    [JsonIgnore]
+    public virtual Category Category { get; set; }
+}
